eb862cb828a5b1086ae8e7ab2e41c37a9165db54
jiyoon127 <jiyoon127@naver.com>
2020-04-08 19:19:15 +0900

    gradient button color without animation by using react-native-linear-gradient

--- MICButton.tsx
+++ MICButton.tsx
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableHighlight} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  Animated,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 interface Props {}
 interface State {
   diameter: number;
 }
+// const AnimatedGradient = Animated.createAnimatedComponent(LinearGradient);
 
 class MICButton extends Component<Props, State> {
   state: State = {
     diameter: 80,
   };
+
+  // midColor = new Animated.Value(0);
+  // lastColor = new Animated.Value(0);
+  // Animated.parallel([
+  //   Animated.timing(this.midColor,{
+
+  //   })
+  // ])
   render() {
     const styles = StyleSheet.create({
       view: {
@@ -23,7 +39,7 @@ class MICButton extends Component<Props, State> {
         height: this.state.diameter,
         width: this.state.diameter,
         borderRadius: 64,
-        backgroundColor: '#342ead',
+        // backgroundColor: '#342ead',
         shadowColor: '#1c0fff',
         shadowOffset: {width: 0, height: 10},
         shadowOpacity: 0.4,
@@ -32,11 +48,17 @@ class MICButton extends Component<Props, State> {
     });
     return (
       <View style={styles.view}>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => alert('clicked')}>
+        <LinearGradient
+          start={{x: 0.0, y: 0.25}}
+          end={{x: 0.5, y: 1.0}}
+          locations={[0, 0.6]}
+          useAngle={true}
+          angle={45}
+          angleCenter={{x: 0.5, y: 0.5}}
+          colors={['#e66465', '#9198e5']}
+          style={styles.button}>
           <Text style={{color: 'white'}}>hey</Text>
-        </TouchableHighlight>
+        </LinearGradient>
       </View>
     );
   }
