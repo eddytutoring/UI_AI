9737a9a085cf8dcf5877dd1e95d3454500e882b8
jiyoon127 <jiyoon127@naver.com>
2020-04-09 11:59:11 +0900

    apply mic image and shadow on linear-gradient button

--- MICButton.tsx
+++ MICButton.tsx
@@ -5,6 +5,9 @@ import {
   Text,
   TouchableHighlight,
   Animated,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -16,7 +19,7 @@ interface State {
 
 class MICButton extends Component<Props, State> {
   state: State = {
-    diameter: 80,
+    diameter: 90,
   };
 
   // midColor = new Animated.Value(0);
@@ -30,7 +33,7 @@ class MICButton extends Component<Props, State> {
     const styles = StyleSheet.create({
       view: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
       },
       button: {
@@ -38,27 +41,37 @@ class MICButton extends Component<Props, State> {
         justifyContent: 'center',
         height: this.state.diameter,
         width: this.state.diameter,
-        borderRadius: 64,
-        // backgroundColor: '#342ead',
-        shadowColor: '#1c0fff',
-        shadowOffset: {width: 0, height: 10},
-        shadowOpacity: 0.4,
-        shadowRadius: 10,
+        borderRadius: 100,
+      },
+      shadow: {
+        shadowColor: '#1a0fff',
+        shadowOffset: {width: 0, height: 8},
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        backgroundColor: 'transparent',
       },
     });
     return (
       <View style={styles.view}>
-        <LinearGradient
-          start={{x: 0.0, y: 0.25}}
-          end={{x: 0.5, y: 1.0}}
-          locations={[0, 0.6]}
-          useAngle={true}
-          angle={45}
-          angleCenter={{x: 0.5, y: 0.5}}
-          colors={['#e66465', '#9198e5']}
-          style={styles.button}>
-          <Text style={{color: 'white'}}>hey</Text>
-        </LinearGradient>
+        <View style={styles.shadow}>
+          <LinearGradient
+            start={{x: 0.0, y: 0.0}}
+            end={{x: 1.0, y: 1.0}}
+            locations={[0.1, 0.7]}
+            useAngle={true}
+            angle={135}
+            angleCenter={{x: 0.5, y: 0.5}}
+            colors={['#e66465', '#9198e5']}
+            // colors={['#fff']}
+            style={styles.button}>
+            {/* <Text style={{color: 'white', backgroundColor: 'transparent'}}>
+              hey
+            </Text> */}
+            <Image
+              source={require('./sources/mic.png')}
+              style={{width: 20, height: 40}}></Image>
+          </LinearGradient>
+        </View>
       </View>
     );
   }
