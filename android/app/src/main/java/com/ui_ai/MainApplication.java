cf7d5ca4b72d1e84b33be15fe1812f45949b9fb3
jiyoon127 <jiyoon127@naver.com>
2020-04-09 18:03:05 +0900

    android radial gradient setting

--- android/app/src/main/java/com/ui_ai/MainApplication.java
+++ android/app/src/main/java/com/ui_ai/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.surajit.rnrg.RNRadialGradientPackage;
 import com.surajit.rnrg.RNRadialGradientPackage;
 import com.surajit.rnrg.RNRadialGradientPackage;
+import com.surajit.rnrg.RNRadialGradientPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -15,6 +16,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.surajit.rnrg.RNRadialGradientPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+//          packages.add(new RNRadialGradientPackage());//TODO:CHECK
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
